8485c422948b6780514b107b4e83749679174ed5
mani3354488 <mani3354488@gmail.com>
2020-11-29 09:47:30 +0530

    writing graphql

---
+++ api-gateway/src/graphql/resolvers/index.js
@@ -0,0 +1,7 @@
+import * as Mutation from "./Mutation";
+import * as Query from "./Query";
+import UserSession from "./userSession";
+
+const resolvers = { Mutation, Query, UserSession };
+
+export default resolvers;
